ded29e6b7ac0eed10763be841511b7e78bbbe615
Denali Balser <denali.balser167@gmail.com>
2019-04-26 19:09:34 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -28,3 +28,23 @@ def bulk_tweet_shortener(tweet)
     puts word_substituter(phrase)
   end
 end
+
+def selective_tweet_shortener(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)
+    elsif phrase.length <= 140
+      phrase
+    end
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)[0..140] + "..."
+    else
+      phrase
+    end
+  end.join(" ")
+end
